16ad53c94b717c5d95283324b12a529f12d2a49f
pranavyuvarajr <pranavyuvarajr@gmail.com>
2026-09-09 13:49:00 +0530

    url for the deployement has been changed and db url changed in backend

--- complaint-management-frontend/src/services/api.js
+++ complaint-management-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://10.10.181.148:8080';
+const API_BASE = 'http://152.67.3.122:8080';
 
 const api = axios.create({
   baseURL: API_BASE + '/api',
