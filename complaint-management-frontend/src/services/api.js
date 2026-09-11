7fc4d0b9f3d60e2e359551fa6f8bcd9c8b60af79
pranavyuvarajr <pranavyuvarajr@gmail.com>
2026-09-11 15:13:30 +0530

    Backend server was certified with HTTPS

--- complaint-management-frontend/src/services/api.js
+++ complaint-management-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://leisure-jumping-icq-recipient.trycloudflare.com';
+const API_BASE = 'https://152.67.3.122';
 
 const api = axios.create({
   baseURL: API_BASE + '/api',
